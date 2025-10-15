15b82351b888ff75ee47e41d9cdfaa1a48a95de4
Oliver Walters <oliver.henry.walters@gmail.com>
2025-10-15 09:21:23 +0000

    Better selection of "oldest" item
    - Consider items which have never been "counted"
    - Annotate the "oldest" history date
    - Use this date in the case where there is no stocktake information

--- rolling_stocktake/serializers.py
+++ rolling_stocktake/serializers.py
@@ -16,10 +16,20 @@ class RollingStocktakeSerializer(serializers.Serializer):
 
         fields = [
             "item",
+            "stocktake_date",
+            "creation_date",
         ]
 
     item = StockItemSerializer(
         many=False,
         read_only=True,
         allow_null=True,
+    )
+
+    stocktake_date = serializers.DateField(
+        source="item.stocktake_date", read_only=True, allow_null=True
+    )
+
+    creation_date = serializers.DateField(
+        source="item.creation_date", read_only=True, allow_null=True
     )
